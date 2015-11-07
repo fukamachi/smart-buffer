9c16a3be6c57a9744dbb9bbb21fcf32fe851f588
Eitaro Fukamachi <e.arrows@gmail.com>
2015-11-08 06:56:55 +0900

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,34 @@
+# smart-io
+
+Smart-io provides an output buffer which changes the destination depending on content size.
+
+* In-memory buffer for small data
+* Temporary files on disk for big data
+
+In-memory buffer is fast to read/write, however, it consumes much memory if the data can be large. On the other hand, files on disk are slower. Smart-io would be useful when the buffer must satisfy these two contradicting demands.
+
+## Usage
+
+`with-smart-buffer` returns an in-memory stream or a file stream.
+
+```common-lisp
+(with-smart-buffer (buffer)
+  (write-to-buffer buffer (flex:string-to-octets "foobar")))
+;=> #<FLEXI-STREAMS::VECTOR-INPUT-STREAM {100456A9A3}>
+
+(with-smart-buffer (buffer :memory-limit 3)
+  (write-to-buffer buffer (flex:string-to-octets "foobar")))
+;=> #<SB-SYS:FD-STREAM for "file /private/var/folders/x3/8n07clk15tq3m4y6_yrmjy6c0000gn/T/tmpAR3FSGEY.tmp" {1005D06503}>
+```
+
+## Author
+
+* Eitaro Fukamachi (e.arrows@gmail.com)
+
+## Copyright
+
+Copyright (c) 2015 Eitaro Fukamachi (e.arrows@gmail.com)
+
+## License
+
+Licensed under the BSD 3-Clause License.
---
+++ src/smart-io.lisp
@@ -0,0 +1,94 @@
+(in-package :cl-user)
+(defpackage smart-io
+  (:use #:cl
+        #:xsubseq)
+  (:export #:*default-memory-limit*
+           #:default-disk-limit*
+
+           #:smart-buffer
+           #:make-smart-buffer
+           #:write-to-buffer
+           #:finalize-buffer
+           #:with-smart-buffer
+
+           #:buffer-limit-exceeded))
+(in-package :smart-io)
+
+(defvar *default-memory-limit* (expt 2 20))
+(defvar *default-disk-limit* (expt 2 30))
+
+(defstruct (smart-buffer (:conc-name :buffer-)
+                         (:constructor %make-smart-buffer))
+  (memory-limit *default-memory-limit*)
+  (disk-limit *default-disk-limit*)
+  (current-len 0)
+  (on-memory-p t)
+  (memory-buffer (make-concatenated-xsubseqs))
+  (disk-buffer nil))
+
+(defun make-smart-buffer (&rest initargs &key memory-limit disk-limit &allow-other-keys)
+  (let ((buffer (apply #'%make-smart-buffer initargs)))
+    (when (and memory-limit
+               disk-limit
+               (< disk-limit memory-limit))
+      (setf (buffer-memory-limit buffer) disk-limit))
+    buffer))
+
+(define-condition buffer-limit-exceeded (error)
+  ((limit :initarg :limit
+          :initform nil))
+  (:report (lambda (condition stream)
+             (format stream "Buffer exceeded the limit~:[~;~:*: ~A~]"
+                     (slot-value condition 'limit)))))
+
+(defun write-to-buffer (buffer seq &optional (start 0) (end (length seq)))
+  (check-type seq (array (unsigned-byte 8) (*)))
+  (incf (buffer-current-len buffer) (- end start))
+  (check-limit buffer)
+  (if (buffer-on-memory-p buffer)
+      (xnconcf (buffer-memory-buffer buffer) (xsubseq seq start end))
+      (with-open-file (out (buffer-disk-buffer buffer)
+                           :direction :output
+                           :element-type '(unsigned-byte 8)
+                           :if-exists :append)
+        (write-sequence seq out :start start :end end))))
+
+(defun check-limit (buffer)
+  (cond
+    ((and (buffer-on-memory-p buffer)
+          (< (buffer-memory-limit buffer)
+             (buffer-current-len buffer)))
+     (when (< (buffer-disk-limit buffer)
+              (buffer-current-len buffer))
+       (error 'buffer-limit-exceeded :limit (buffer-disk-limit buffer)))
+     (setf (buffer-disk-buffer buffer)
+           (uiop:with-temporary-file (:stream stream :pathname tmp
+                                      :direction :output
+                                      :element-type '(unsigned-byte 8)
+                                      :keep t)
+             (typecase (buffer-memory-buffer buffer)
+               (null-concatenated-xsubseqs)
+               (t (write-sequence (coerce-to-sequence (buffer-memory-buffer buffer)) stream)))
+             tmp)
+           (buffer-on-memory-p buffer) nil
+           (buffer-memory-buffer buffer) nil))
+    ((and (not (buffer-on-memory-p buffer))
+          (< (buffer-disk-limit buffer)
+             (buffer-current-len buffer)))
+     (error 'buffer-limit-exceeded :limit (buffer-disk-limit buffer)))))
+
+(defun finalize-buffer (buffer)
+  (if (buffer-on-memory-p buffer)
+      (flex:make-in-memory-input-stream
+       (typecase (buffer-memory-buffer buffer)
+         (null-concatenated-xsubseqs #())
+         (t (coerce-to-sequence (buffer-memory-buffer buffer)))))
+      (open (buffer-disk-buffer buffer) :direction :input :element-type '(unsigned-byte 8))))
+
+(defmacro with-smart-buffer ((buffer &key
+                                       (memory-limit '*default-memory-limit*)
+                                       (disk-limit '*default-disk-limit*))
+                             &body body)
+  `(let ((,buffer (make-smart-buffer :memory-limit ,memory-limit :disk-limit ,disk-limit)))
+     ,@body
+     (finalize-buffer ,buffer)))
